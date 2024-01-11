9c7d0187db19fbaf63fb39aff4b1d07be105f3c6
Elijah <kcerhajile@gmail.com>
2024-01-11 15:20:29 -0500

    Added: win/loss state and best score

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ function randomNum(min, max){
 }
 
 function toProperCase(string){
-  //return string.slice(0,1).toUpperCase() + string.slice(1)
-  return string
+  return string.slice(0,1).toUpperCase() + string.slice(1)
   
 }
 
@@ -17,35 +16,58 @@ function App() {
   const [pkmnArray, setPkmnArray] = useState([])
   const [gameState, setGameState] = useState(0) 
   const [done, setDone] = useState(false)
+  const [bestScore, setBestScore] = useState(0)
 
+
+  const getNewPkmn = async (amount = 12) => {
+    let newPkmnArray = [];
+    let usedId = [];
+    for(let i = 0; i < amount; i++){
+      let randomId = randomNum(1,1018)
+      while(usedId.includes(randomId)){
+        randomId = randomNum(1,1018)
+      }
+      usedId.push(randomId)
+      let response = await fetch(pkmnUrl + randomId)
+      let data = await response.json()
+      let newPkmn = {
+        id:data.id,
+        name:data.name,
+        sprite: data.sprites["front_default"],
+        guessed: false
+      }
+      newPkmnArray.push(newPkmn)
+    }
+    setPkmnArray(newPkmnArray)
+    setDone(true)
+  }
 
   useEffect(()=>{
-    const getNewPkmn = async (amount = 12) => {
-      let newPkmnArray = [];
-      let usedId = [];
-      for(let i = 0; i < amount; i++){
-        let randomId = randomNum(1,1018)
-        while(usedId.includes(randomId)){
-          randomId = randomNum(1,1018)
-        }
-        usedId.push(randomId)
-        let response = await fetch(pkmnUrl + randomId)
-        let data = await response.json()
-        let newPkmn = {
-          id:data.id,
-          name:data.name,
-          sprite: data.sprites["front_default"],
-          guessed: false
-        }
-        newPkmnArray.push(newPkmn)
-      }
-      setPkmnArray(newPkmnArray)
-      setDone(true)
-    }
-    gameState && getNewPkmn()
+    gameState == 1 && getNewPkmn()
   },[gameState])
 
+  let totalGuesses = pkmnArray.reduce((totalSelected, pkmn)=>{
+    if(pkmn.guessed){
+      return totalSelected + 1
+    } else{
+      return totalSelected
+    }
+  },0)
+
+  useEffect(()=>{
+    if(totalGuesses === pkmnArray.length){
+      setGameState(2)
+    }
+  },[totalGuesses, pkmnArray])
+
+  useEffect(()=>{
+    if(totalGuesses > bestScore){
+      setBestScore(totalGuesses)
+    }
+  },[totalGuesses, bestScore])
+
   const handleGameState = (newState) => {
+    setDone(false)
     setGameState(newState)
   }
 
@@ -55,7 +77,7 @@ function App() {
       randomSort(id)
     } else {
       //for testing purposes
-      alert('lose')
+      handleGameState(2)
     }
   }
 
@@ -75,13 +97,11 @@ function App() {
     }
   }
 
-  let totalGuesses = pkmnArray.reduce((totalSelected, pkmn)=>{
-    if(pkmn.guessed){
-      return totalSelected + 1
-    } else{
-      return totalSelected
-    }
-  },0)
+  const resetGame = () => {
+    handleGameState(1)
+  }
+
+  
   
   
 
@@ -99,7 +119,10 @@ function App() {
             )
           })}
         </div>
-        <div id="stats">Score: {totalGuesses}/{pkmnArray.length}</div>
+        <div id="stats">
+          <p>Score: {totalGuesses}/{pkmnArray.length}</p>
+          <p>Best Score: {bestScore}</p>
+        </div>
       </div>
       
     )
@@ -112,6 +135,23 @@ function App() {
     return (
       <button onClick={()=>handleGameState(1)}>Start</button>
     )
+  } else if(gameState === 2){
+    if(totalGuesses === pkmnArray.length){
+      return (
+        <div className="result">
+          <h1>You win!</h1>
+          <button type="button" onClick={resetGame}>Play again?</button>
+        </div>
+      )
+    } else{
+      return (
+        <div className="result">
+          <h1>You lose!</h1>
+          <p>Your Score: {totalGuesses}</p>
+          <button type="button" onClick={resetGame}>Try again?</button>
+        </div>
+      )
+    }
   }
 }
 
